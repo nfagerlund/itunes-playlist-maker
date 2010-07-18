be7b536e16e60d139a36f85d4c58b0913699e6dd
Nick Fagerlund <nick.fagerlund@gmail.com>
2010-07-17 21:29:12 -0700

    I fully expect this to still be broken, but I've enabled perl5i autoboxing within the handler package, I've made it a proper subclass of XML::SAX::Base, and I thiiiiiink I've got the attributes set up properly.

--- yackety-sax.pl
+++ yackety-sax.pl
@@ -1,18 +1,23 @@
 #!/usr/bin/env perl
 
 
-
-
-
 use perl5i::2;
 use Modern::Perl;
 use XML::SAX;
-# use SaxTrackHandler; HURF DURF, not needed. 
+
+
 
 my %ArtistAlbums;
 my %Compilations;
 
-my $parser = XML::SAX::ParserFactory->parser( Handler => SaxTrackHandler->new(ArtistAlbumsShelf => \%ArtistAlbums, CompilationsShelf => \%Compilations));
+
+
+
+my $parser = 
+	XML::SAX::ParserFactory->parser( 
+		Handler => SaxTrackHandler->new(ArtistAlbumsShelf => \%ArtistAlbums, CompilationsShelf => \%Compilations)
+	);
+	
 $parser->parse_uri("/Users/nick/Desktop/complete albums/testdata.xml");
 
 # And now some testing code:
@@ -20,18 +25,19 @@ say %Compilations->mo->perl;
 say "Okay, and now the artist albums: ";
 say %ArtistAlbums->mo->perl;
 
+
+
+
+
+
+
 package SaxTrackHandler;
-
-
-# 	use perl5i::2; Uh, not for now? Let's try that again. 
-	use Modern::Perl;
-	use XML::SAX::Base;
+#	use Modern::Perl;
+	use perl5i::2;
 	use Moose;
+	BEGIN { extends 'XML::SAX::Base'; }
+
 # 	use XML::Filter::BufferText;
-	extends qw( Moose::Object XML::SAX::Base ); # This MIGHT be necessary? 
-# 	extends 'XML::SAX::Base';
-	# But I like this better because I distrust multi-parent households. We'll have to introspect later and see if my fave version actually works. 
-	
 # 	sub BUILD {
 # 		my $self = shift;
 # 		return XML::Filter::BufferText->new( Handler => $self );
@@ -39,29 +45,30 @@ package SaxTrackHandler;
 	# Okay, let's see if that works. 
 	# It does! At least in the sense that it doesn't break things. I think. We'll have to check efficacy later. 
 	
-	my $last_element = '';
-	my $current_element = '';
-	my $current_key = '';
-	my @dict_state = (); # Push key names onto me when we enter a dict. Once we pop Tracks, we can kill the whole project. Maybe. If it's possible. 
-	my %current_track_record = (); 
 	
-	# Is all the above scoped to the specific object instance? I can't tell! Ask Schwern. 
+	# PUBLIC ATTRIBUTES:
+	has 'ArtistAlbumsShelf' => (is => 'ro', isa => 'HashRef');
+	has 'CompilationsShelf' => (is => 'ro', isa => 'HashRef');
 	
-	has 'ArtistAlbumsShelf', is => 'ro', isa => 'HashRef';
-	has 'CompilationsShelf', is => 'ro', isa => 'HashRef';
-	# re-enable these as soon as you're ready to start doing anything interesting w/ this data. 
-	# Also, is there any way we can get this unified? Geez. 
-	# Maybe preserve the "compilations" flag and put them all under an artist called "various."
+	# PRIVATE ATTRIBUTES:
+	has last_element => ( is => 'rw', default => '', init_arg => undef );
+	has current_element => ( is => 'rw', default => '', init_arg => undef );
+	has current_key => ( is => 'rw', default => '', init_arg => undef );
+	has dict_state => ( is => 'rw', default => sub{ [] }, isa => 'ArrayRef', init_arg => undef );
+	has active_track_record => ( is => 'rw', default => sub{ {} }, isa => 'HashRef', init_arg => undef );
+	
 	
 	sub start_element {
 		my ($self, $element_structure) = @_;
-		$current_element = $element_structure->{'LocalName'};
-		if ($current_element eq 'dict') {
-			if ($last_element eq 'plist') {
-				push(@dict_state, 'plist');
+		$self->current_element(
+			$element_structure->{'LocalName'}
+		);
+		if ($self->current_element eq 'dict') {
+			if ($self->last_element eq 'plist') {
+				$self->dict_state->push('plist');
 			}
 			else {
-				push(@dict_state, $current_key);
+				$self->dict_state->push($self->current_key);
 			}
 		}
 
@@ -75,17 +82,16 @@ package SaxTrackHandler;
 	
 	sub characters {
 		my ($self, $data_hashref) = @_;
-		# say $data_hashref->{'Data'};
-		given ($current_element) {
+		given ($self->current_element) {
 			when ('key') {
-				$current_key = $data_hashref->{'Data'};
+				$self->current_key( $data_hashref->{'Data'} );
 			}
 			when ('dict') { 
 				return; # Because I think there's a bunch of whitespace I'd like to avoid trying to do a hash lookup on. 
 			}
 			default {
-				return unless $dict_state[1] eq 'Tracks';
-				$current_track_record{$current_key} = $data_hashref->{'Data'};
+				return unless $self->dict_state->[1] eq 'Tracks';
+				$self->active_track_record->{$self->current_key} = $data_hashref->{'Data'};
 			}
 		}
 		
@@ -95,31 +101,33 @@ package SaxTrackHandler;
 	
 	sub end_element {
 		my ($self, $element_structure) = @_;
-		$last_element = $element_structure->{'LocalName'};
-		$current_element = '';
+		$self->last_element( $element_structure->{'LocalName'} );
+		$self->current_element('');
 		
-		given ($last_element) {
+		given ($self->last_element) {
 			# special cases for the empty values:
 			when ('true') {
-				$current_track_record{$current_key} = 'true';
+				$self->active_track_record->{$self->current_key} = 'true';
 			}
 			when ('false') {
-				$current_track_record{$current_key} = '';
+				$self->active_track_record->{$self->current_key} = '';
 			}
 			when ('dict') {
-				my $just_finished_dict = pop(@dict_state);
+				my $just_finished_dict = $self->dict_state->pop;
 				# If $just_finished_dict eq 'Tracks', we fuckin' bail, except I don't know how to do that yet. 
-				# Else (i.e. $just_finished_dict =~ /\d+/ and $dict_state[-1] eq 'Tracks'), we have a complete track record! Check for sanity per the old meatpacking loop, and put it on the shelf if it passes. Finally, set %current_track_record = ().
+				# Else (i.e. $just_finished_dict =~ /\d+/ and $dict_state[-1] eq 'Tracks'), we have a complete track record! Check for sanity per the old meatpacking loop, and put it on the shelf if it passes. Finally, set %active_track_record = ().
 				
-				if ($just_finished_dict =~ /\d+/ and $dict_state[-1] eq 'Tracks') {
-					return unless ( $current_track_record{'Album'} and $current_track_record{'Track Count'} and $current_track_record{'Track Count'} );
-					$ArtistAlbumsShelf->{ $current_track_record{'Artist'} } ||= {};
-					my $shelf = 
-						($current_track_record{'Compilation'} ? $CompilationsShelf : $ArtistAlbumsShelf->{ $current_track_record{'Artist'} });
-					my $album = $shelf->{ $current_track_record{'Album'} };
-					$album->{tracks_array}[ $current_track_record{'Track Number'} - 1 ] = 1;
-					$album->{total_tracks} //= $current_track_record{'Track Count'}; #/
-					$album->{length_in_milliseconds} += $current_track_record{'Total Time'};
+				if ($just_finished_dict =~ /\d+/ and $self->dict_state->[-1] eq 'Tracks') {
+					return unless ( $self->active_track_record->{'Album'} and $self->active_track_record->{'Track Count'} and $self->active_track_record->{'Track Number'} );
+					$self->ArtistAlbumsShelf->{ $self->active_track_record{'Artist'} } ||= {};
+					my $temp_shelf = ($self->active_track_record->{'Compilation'} ? 
+						$self->CompilationsShelf : 
+						$self->ArtistAlbumsShelf->{ $active_track_record{'Artist'} }
+					);
+					my $album = $temp_shelf->{ $self->active_track_record->{'Album'} };
+					$album->{tracks_array}[ $self->active_track_record->{'Track Number'} - 1 ] = 1;
+					$album->{total_tracks} = $self->active_track_record->{'Track Count'}; 
+					$album->{length_in_milliseconds} += $self->active_track_record->{'Total Time'};
 					
 				}
 			}
