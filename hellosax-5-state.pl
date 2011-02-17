47b2d4436603a9c09234e2dcb053e47a777d47a5
Nick Fagerlund <nick.fagerlund@gmail.com>
2011-02-17 01:26:48 -0800

    If I push something on within the characters sub, it  works...

--- hellosax-5-state.pl
+++ hellosax-5-state.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use XML::SAX;
 use perl5i::2;
-use modern::perl;
+# use modern::perl; # This is throwing an error today, dunno what's up. 
 
 my $handler = TestSAXHandler->new();
 my $parser = XML::SAX::ParserFactory->parser(
@@ -15,11 +15,11 @@ say $a_number;
 package TestSAXHandler;
 use base qw(XML::SAX::Base);
 use perl5i::2;
-use modern::perl;
+# use modern::perl;
 
 my $elementcount = 0;
 my $integercount = 0; 
-my @elementstack = qw(item one two three);
+my @elementstack;
 
 sub start_element {
     my ($self, $element_structure) = @_;
@@ -32,8 +32,9 @@ sub end_element {}
 
 sub characters {
     my ($self, $characters_structure) = @_;
+    @elementstack->push("extra-fakey");
     my $obvioustempvar = join(', ', @elementstack);
-    say "fakie" . $integercount if $characters_structure->{'Data'} eq 'Brighter Days';
+    say "fakie" . $integercount . $elementstack[1] if $characters_structure->{'Data'} eq 'Brighter Days';
     # this works, but I can't get $elementstack[0] to work here.
 }
 
