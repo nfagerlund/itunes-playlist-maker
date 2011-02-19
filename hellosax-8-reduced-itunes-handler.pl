4190300e0269571b8478ca87b3a51e248270c828
Nick Fagerlund <nick.fagerlund@gmail.com>
2011-02-19 02:44:36 -0800

    Success! We can tell whether we're inside a track

--- hellosax-8-reduced-itunes-handler.pl
+++ hellosax-8-reduced-itunes-handler.pl
@@ -27,7 +27,7 @@ my @element_stack;
 my @key_stack;
 my @data_structure_stack;
 my $inside_tracks_dict;
-my $inside_some_track; # dunno if I'll use either of those. 
+my $inside_some_track;
 
 # our throwaway var for track names
 my @track_names;
@@ -46,7 +46,7 @@ sub start_element {
         when (/(true|false)/) { 
             # do something, because we won't get a characters event. 
             # And I think it'll always be a value to a key. Can't see any reason to have an anonymous bool. 
-            say "found a bool $_";
+            say "found a bool $_"; # test code
         }
     }
 }
@@ -66,7 +66,7 @@ sub end_element {
         { $self->exit_dict_or_array; }
     }
     
-    @element_stack->shift; # This should be the last thing we do, so our hungry key method works the same in characters and end_element.
+    @element_stack->shift; # This should be the last thing we do, so our hungry key method if we decide to make one works the same in characters and end_element.
 }
 
 sub characters {
@@ -119,7 +119,16 @@ sub enter_dict {
     if ($self->in_a_dict)
     {
         @data_structure_stack->unshift({ name => $key_stack[0], type => 'dict'});
-        $inside_tracks_dict = 1 if $key_stack[0] eq 'Tracks';
+        if ($inside_tracks_dict == 1)
+        {
+            # The keys for tracks will all be \d+, but I think we can ignore that.
+            $inside_some_track = 1;
+            say "Entering track " . $key_stack[0]; # test code
+        }
+        elsif ($key_stack[0] eq 'Tracks')
+        {
+            $inside_tracks_dict = 1;
+        }
     }
     else
     {
@@ -142,9 +151,12 @@ sub enter_array {
 sub exit_dict_or_array {
     my ($self) = @_;
     my $erstwhile_structure = @data_structure_stack->shift; 
-    given ($erstwhile_structure->{name}) 
+    if ($inside_tracks_dict == 1)
     {
-        when ('Tracks') { $inside_tracks_dict = -1; }
+        if ($erstwhile_structure->{name} eq 'Tracks') { $inside_tracks_dict = -1; }
+        else { $inside_some_track = 0; 
+            say "Leaving track " . $erstwhile_structure->{name}; # test code
+        }
     }
 }
 
